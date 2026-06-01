fb51191096f6b4b4b51457d7e80dd87d6c392fb9
infuzoriyadashenka-del <infuzoriyadashenka@gmail.com>
2026-06-01 21:31:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,47 +13,31 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-print("BOT_TOKEN =", BOT_TOKEN)
-
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN is missing")
 
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s | %(levelname)s | %(message)s"
-)
+logging.basicConfig(level=logging.INFO)
+
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 
-async def startup():
-    logging.info("Initializing database...")
+async def main():
+
+    logging.info("init db...")
     await init_db()
 
-    logging.info("Connecting router...")
+    logging.info("router...")
     dp.include_router(router)
 
-    logging.info("Starting scheduler...")
+    logging.info("scheduler...")
     setup_scheduler(bot)
 
-    logging.info("Bot initialized")
+    logging.info("start polling...")
 
-
-async def main():
-    await startup()
-
-    logging.info("Removing webhook...")
-
-    await bot.delete_webhook(drop_pending_updates=True)
-
-    logging.info("Start polling...")
-
-    await dp.start_polling(
-        bot,
-        allowed_updates=["message"]
-    )
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
